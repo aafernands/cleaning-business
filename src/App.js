10755247701980b61d4cd0b8e909834aac92d1a0
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-10-01 15:16:12 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 import NavTabs from "./shared/components/NavTabs/NavTabs";
@@ -23,16 +23,18 @@ function App() {
 	const classes = userStyles();
 	return (
 		<Router>
-			<Container className={classes.test}>
-				<NavTabs />
-				<Route exact path="/" component={Home} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/contact" component={Contact} />
-				<Route path="/services" component={Services} />
-				<Route path="/footer" component={Footer} />
-				<Route path="/submitted" component={Submitted} />
-				<Footer />
-			</Container>
+			<Switch>
+				<Container className={classes.test}>
+					<NavTabs />
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/contact" component={Contact} />
+					<Route path="/services" component={Services} />
+					<Route path="/footer" component={Footer} />
+					<Route path="/submitted" component={Submitted} />
+					<Footer />
+				</Container>
+			</Switch>
 		</Router>
 	);
 }
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,62 +1,128 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Box } from "@material-ui/core";
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
-import "./HomePage.css";
-import InfoCard from "./components/infoCard/infoCard";
-import Particles from "react-particles-js";
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const userStyles = makeStyles((theme) => ({
-	root: {
-		position: "relative",
-		color: "white",
-		background: "#000000ea;",
-	},
-	particles: {
-		position: "absolute",
-		top: 0,
-		left: 0,
-		width: "100%",
-		height: "100%",
-		// height: 620,
-	},
-	paragraph: {
-		width: 500,
-	},
-}));
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
 
-function Home() {
-	const classes = userStyles();
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
 
-	return (
-		<div className={classes.root}>
-			<Particles
-				className={classes.particles}
-				params={{
-					particles: {
-						number: {
-							value: 70,
-						},
-					},
-				}}
-			>
-				<h1>testing</h1>
-			</Particles>
-			<Box alignItems="center" justifyContent="center">
-				<div className="home">
-					<Grid
-						container
-						direction="column"
-						justify="center"
-						alignItems="center"
-					>
-							<InfoCard />
- 						
-					</Grid>
-				</div>
-			</Box>
-		</div>
-	);
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
 }
 
-export default Home;
+export default SwipeableTextMobileStepper;
