db86cc0ebe3919546d88b301fed956bff19bfc27
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-10-18 23:13:32 -0400

    updates

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -14,12 +14,10 @@ import done from "./done.jpeg";
 function SwipeableTextMobileStepper() {
 	return (
 		<Container>
-			<br></br>
 			<Grid container spacing={2}>
 				<Grid item xs={12} md={8} lg={8}>
 					<Carousel />
 				</Grid>
-
 				<Grid item xs={12} md={4} lg={4}>
 					<ListHomePage />
 				</Grid>
